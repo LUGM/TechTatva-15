5463fcac688a1009d4f04943a1c4fab49b2597ce
ykl7 <yklal95@gmail.com>
2015-06-25 15:45:57 +0530

    revamping of some views

---
+++ TechTatva15/MakeAccountViewController.h
@@ -0,0 +1,13 @@
+//
+//  MakeAccountViewController.h
+//  TechTatva15
+//
+//  Created by YASH on 25/06/15.
+//  Copyright (c) 2015 AppDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MakeAccountViewController : UITableViewController
+
+@end
