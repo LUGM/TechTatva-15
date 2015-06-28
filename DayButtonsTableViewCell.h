ae32007833694a5919b89adfc9b49eea7df2c077
ykl7 <yklal95@gmail.com>
2015-06-28 13:54:52 +0530

    Files reaaranged

---
+++ DayButtonsTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  DayButtonsTableViewCell.h
+//  TechTatva15
+//
+//  Created by YASH on 28/06/15.
+//  Copyright (c) 2015 AppDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DayButtonsTableViewCell : UITableViewCell
+
+@end
---
+++ DayButtonsTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  DayButtonsTableViewCell.m
+//  TechTatva15
+//
+//  Created by YASH on 28/06/15.
+//  Copyright (c) 2015 AppDev. All rights reserved.
+//
+
+#import "DayButtonsTableViewCell.h"
+
+@implementation DayButtonsTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
