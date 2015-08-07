24dd3e350e01aecf94dab77ab98377c655ff8fc7
Sushant Gakhar <gakhasushant@gmail.com>
2015-08-08 01:36:56 +0530

    Made Event Library

---
+++ EventDataModel.h
@@ -0,0 +1,29 @@
+//
+//  EventDataModel.h
+//  TechTatva15
+//
+//  Created by Sushant Gakhar on 08/08/15.
+//  Copyright (c) 2015 AppDev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EventDataModel : NSObject
+
+@property (strong, nonatomic) NSString *eventName;
+@property (strong, nonatomic) NSNumber *eventID;
+@property (strong, nonatomic) NSString *eventDescription;
+@property (strong, nonatomic) NSString *categoryName;
+@property (strong, nonatomic) NSNumber *categoryID;
+@property (strong, nonatomic) NSNumber *maximumNumberOfMembersInATeam;
+@property (strong, nonatomic) NSNumber *startTiming;
+@property (strong, nonatomic) NSNumber *endTiming;
+@property (strong, nonatomic) NSNumber *contactNumber;
+@property (strong, nonatomic) NSString *results;
+@property (strong, nonatomic) NSString *personToContact;
+@property  BOOL isFavourite;
+
+
++(EventDataModel*) createEventWithName:(NSString *)Event_Name EventID:(NSNumber *)Event_ID eventDescription:(NSString *)Event_Description:(NSString *)Category_Name CategoryID:(NSNumber *)Category_ID Maximum_Number_Of_MembersInATeam: (NSNumber*) Maximum_Number_Of_Mumbers_In_A_Team StartingTime :(NSNumber *)Event_Starting_Team :(NSNumber *)Event_Staring_Time Event_Ending_Time:(NSNumber *)Event_Ending_Time Contact_Number:(NSNumber *)Contact_Number PersonToContact : (NSString*)Person_To_Contact results:(NSString *)Event_Results;
+
+@end
---
+++ EventDataModel.m
@@ -0,0 +1,34 @@
+//
+//  EventDataModel.m
+//  TechTatva15
+//
+//  Created by Sushant Gakhar on 08/08/15.
+//  Copyright (c) 2015 AppDev. All rights reserved.
+//
+
+#import "EventDataModel.h"
+
+@implementation EventDataModel
+
++(EventDataModel*) createEventWithName:(NSString *)Event_Name EventID:(NSNumber *)Event_ID eventDescription:(NSString *)Event_Description:(NSString *)Category_Name CategoryID:(NSNumber *)Category_ID Maximum_Number_Of_MembersInATeam: (NSNumber*) Maximum_Number_Of_Mumbers_In_A_Team StartingTime :(NSNumber *)Event_Starting_Team :(NSNumber *)Event_Starting_Time Event_Ending_Time:(NSNumber *)Event_Ending_Time Contact_Number:(NSNumber *)Contact_Number PersonToContact : (NSString*)Person_To_Contact results:(NSString *)Event_Results;
+{
+    EventDataModel *event = [[EventDataModel alloc] init];
+    
+    event.eventName = Event_Name;
+    event.eventID = Event_ID;
+    event.eventDescription = Event_Description;
+    event.categoryName = Category_Name;
+    event.categoryID = Category_ID;
+    event.maximumNumberOfMembersInATeam = Maximum_Number_Of_Mumbers_In_A_Team;
+    event.startTiming = Event_Starting_Time;
+    event.endTiming = Event_Ending_Time;
+    event.contactNumber = Contact_Number;
+    event.personToContact = Person_To_Contact;
+    event.results = Event_Results;
+    event.isFavourite = NO;
+    
+    return event;
+    
+}
+
+@end
