aa5f8d9aecfb6191fa390b68fdebbe3ff55ea650
Sushant Gakhar <gakhasushant@gmail.com>
2015-08-30 00:28:44 +0530

    Minor fix. Data loads in events view now.

--- TechTatva15/EventsViewController.m
+++ TechTatva15/EventsViewController.m
@@ -54,18 +54,18 @@
     
     NSURL *eventsUrl;
     
-    if (![self isInternetAvailable])
-    {
-        
+//    if (![self isInternetAvailable])
+//    {
+    
         eventsUrl = [NSURL URLWithString:@"http://api.techtatva.in/events"];
-        
-    }
-    else
-    {
-        
-        eventsUrl = [NSURL URLWithString:@"http://localhost:8888/events.json"];
-        
-    }
+//        
+//    }
+//    else
+//    {
+//        
+//        eventsUrl = [NSURL URLWithString:@"http://localhost:8888/events.json"];
+//        
+//    }
     
     myJsonInstance = [[SSJSONModel alloc] initWithDelegate:self];
     myJsonInstance.delegate = self;
